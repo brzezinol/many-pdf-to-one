44e91b764cfe72f3ec6784271d2d12311f2e62fa
brzezinol@gmail.com <brzezinol@gmail.com@183071b4-95ec-32cc-b70a-45e6d7654973>
2014-12-10 12:04:49 +0000

    First release.

--- manypdftoone/manypdftoone.cs
+++ manypdftoone/manypdftoone.cs
@@ -4,32 +4,43 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace manypdftoone
 {
     public class ManyPdfToOne
     {
         private DirectoryInfo _sourceDir = null;
-        private DirectoryInfo _destinationDir = null;
         private FileInfo[] _sourceFiles = null;
         private FileInfo _destinationFile = null;
         private eMergePagesMode _mergeMode = eMergePagesMode.OneSide;
         private List<SourceFile> _sourceFilesDef = new List<SourceFile>();
         private string _gsfileName = string.Empty;
+        private Regex _rxFileName = new Regex("^[^\\/\\\\:\\*\\?\"\\<\\>\\|()]+$", RegexOptions.IgnoreCase);
 
         /// <summary>
         /// Initialize class with instantiate all objects.
         /// </summary>
         /// <param name="sourceDir">Source directory for find all pdf files</param>
+        /// <param name="destinationFile"></param>
+        public ManyPdfToOne(string sourceDir, string destinationFile, eMergePagesMode mergeMode)
+        {
+            Init(sourceDir, destinationFile, mergeMode);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sourceDir"></param>
         /// <param name="destinationDir"></param>
-        public ManyPdfToOne(string sourceDir, string destinationDir, eMergePagesMode mergeMode)
+        /// <param name="mergeMode"></param>
+        private void Init(string sourceDir, string destinationFile, eMergePagesMode mergeMode)
         {
             _sourceDir = new DirectoryInfo(sourceDir);
-            _destinationDir = new DirectoryInfo(destinationDir);
-
             _sourceFiles = _sourceDir.GetFiles("*.pdf", SearchOption.AllDirectories);
-            _destinationFile = new FileInfo(Path.GetTempFileName());
+            File.Create(destinationFile).Close();
+            _destinationFile = new FileInfo(destinationFile);
+            _mergeMode = mergeMode;
         }
 
         /// <summary>
@@ -37,6 +48,7 @@ namespace manypdftoone
         /// </summary>
         public void Merge()
         {
+            #region check ghostscript
             string gsFileName = string.Empty;
 
             RegistryKey gsKey = Registry.LocalMachine.OpenSubKey("SOFTWARE\\GPL Ghostscript");
@@ -57,20 +69,77 @@ namespace manypdftoone
             if (!new FileInfo(gsFileName).Exists)
                 throw new FileNotFoundException(string.Format("Brak pliku {0}", gsFileName));
 
-            _gsfileName = gsFileName;
+            _gsfileName = gsFileName; 
+            #endregion
+
+            #region Prepare files definition
+            var sortedList = from file in _sourceFiles.ToList()
+                             orderby file.FullName ascending
+                             select file;
+            _sourceFiles = sortedList.ToArray<FileInfo>();
 
             for (int i = 0; i < _sourceFiles.Length; i++)
             {
                 SourceFile sf = new SourceFile();
-                sf.FilePath = _sourceFiles[i].FullName;
+                sf.FilePath = _sourceFiles[i].FullName.Replace("\\", "/").Trim();
                 sf.PagesCount = CountPages(sf.FilePath);
+
+                if (!_rxFileName.IsMatch(new FileInfo(sf.FilePath).Name))
+                    throw new Exception(string.Format("Nieprawidłowa nazwa pliku {0}. Niedozwolone znaki space^/\\:*?\"<>|()", new FileInfo(sf.FilePath).Name));
+
                 _sourceFilesDef.Add(sf);
-                if (sf.PagesCount % 2 != 0)
-                    _sourceFilesDef.Add(GetOddPage());
-            }
+
+                if (_mergeMode == eMergePagesMode.TwoSide && sf.PagesCount % 2 != 0)
+                    _sourceFilesDef.Add(GetEvenPage());
+            } 
+            #endregion
+
+            #region building merge command
+            StringBuilder gsMergeCommand = new StringBuilder()
+                .Append(" -dBATCH")
+                .Append(" -dNOPAUSE")
+                .Append(" -dQUIET")
+                .Append(" -dSHORTERRORS")
+                .Append(" -sDEVICE=pdfwrite")
+                .Append(" -sOutputFile=\"").Append(_destinationFile.FullName.Replace("\\", "/")).Append("\"");
+
+            foreach (SourceFile sf in _sourceFilesDef)
+                gsMergeCommand.Append(" \"").Append(sf.FilePath).Append("\""); 
+            #endregion
+
+            Console.WriteLine(gsMergeCommand.ToString());
+
+            RunMerge(gsMergeCommand.ToString());
         }
 
-        private SourceFile GetOddPage()
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sourceDir"></param>
+        /// <param name="destinationDir"></param>
+        /// <param name="mergeMode"></param>
+        public void Merge(string sourceDir, string destinationDir, eMergePagesMode mergeMode)
+        {
+            Init(sourceDir, destinationDir, mergeMode);
+            Merge();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="command"></param>
+        private void RunMerge(string command)
+        {
+            ShellProcess sp = new ShellProcess();
+            string[] response = sp.Run(_gsfileName, command);
+
+            Console.Write(response[0]);
+
+            if (response[1].ToLower().Contains("error:"))
+                throw new Exception(response[0] + response[1]);
+        }
+
+        private SourceFile GetEvenPage()
         {
             SourceFile sf = new SourceFile();
             sf.FilePath = Path.GetTempFileName();
